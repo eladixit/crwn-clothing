822c8d0615fccab745b09c9cea5d3a6c09de5664
Ela Dixit <ela.mishra02@gmail.com>
2020-06-29 22:00:49 +0530

    added redux , firebase and stripe payment api

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,49 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyA7k_sSLe8fdujjfr2bTBrf9pNmt9au_I4",
+  authDomain: "crwn-db-e1d40.firebaseapp.com",
+  databaseURL: "https://crwn-db-e1d40.firebaseio.com",
+  projectId: "crwn-db-e1d40",
+  storageBucket: "crwn-db-e1d40.appspot.com",
+  messagingSenderId: "151190391423",
+  appId: "1:151190391423:web:648d65b2c4207accea0d4a",
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-import "./homepage.styles.scss";
-import Directory from "../../components/directory/directory.component";
-import MenuItem from "../../components/menu-item/menu-item.component";
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
 
 const HomePage = () => (
-  <div className="homepage">
-    <Directory>
-      <MenuItem></MenuItem>
-    </Directory>
+  <div className='homepage'>
+    <Directory />
   </div>
 );
 
